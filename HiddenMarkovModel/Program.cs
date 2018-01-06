f753b72110595912bc548f2eed0c69be39d6f496
DESKTOP-I2MQO0A\naiwe <naiwenchi0330@gmail.com>
2018-01-07 00:24:06 +0800

    20180106 finish brute force algorithm for illustrating all possible status path. It is an important breakthrough because it was fucking difficult for me. I hate recusive function.

--- HiddenMarkovModel/Program.cs
+++ HiddenMarkovModel/Program.cs
@@ -10,15 +10,90 @@ namespace HiddenMarkovModel
     {
         static void Main(string[] args)
         {
+            //List<string> result = new List<string>();
+            //string chars = "abc";           
+            ////GenerateAllPasswords("", 0, 3, chars, result);
+            ////Console.WriteLine(result.Count);
+
+            //List<string> bitch = new List<string>();
+            //bitch.Add("CP");
+            //bitch.Add("IP");
+
+            //GenerateAllPasswords("", 0, 4, bitch, result);
+            //Console.WriteLine(result.Count);
+
             HMM crazySoftDrinkMachine = new HMM();
-            Path myPath1 = new Path("lem,icet","CP,CP", crazySoftDrinkMachine);
-            double result1=myPath1.calculatePathProb();
-            Console.WriteLine(result1);
-            Path myPath2 = new Path("lem,icet", "CP,IP", crazySoftDrinkMachine);
-            double result2 = myPath2.calculatePathProb();
-            Console.WriteLine(result2);
-            Console.WriteLine("final: "+(result1+result2));
+            //Backtrack brute = new Backtrack(2, crazySoftDrinkMachine.S);
+            Backtrack brute = new Backtrack(4, crazySoftDrinkMachine.S);
+            List<string> result = brute.getALLPaths();
+            //foreach (string str in result)
+            //    Console.WriteLine(str);
+
+            //string observation = "lem,icet";
+            string observation = "lem,icet,cola,lem";
+            double totalObsProb = 0;
+            for (int i = 0; i != result.Count; i++)
+            {
+                Path myPath1 = new Path(observation,result[i], crazySoftDrinkMachine);
+                double result1=myPath1.calculatePathProb();
+                Console.WriteLine("This status path is: " + result[i]);
+                Console.WriteLine("The probablity is: " + result1);
+                totalObsProb += result1;
+            }
+            Console.WriteLine("This observation path is: "+observation);
+            Console.WriteLine("The total probability of this observation path is: " + totalObsProb);
+
+            //Path myPath1 = new Path("lem,icet","CP,CP", crazySoftDrinkMachine);
+            //double result1=myPath1.calculatePathProb();
+            //Console.WriteLine(result1);
+            //Path myPath2 = new Path("lem,icet", "CP,IP", crazySoftDrinkMachine);
+            //double result2 = myPath2.calculatePathProb();
+            //Console.WriteLine(result2);
+            //Console.WriteLine("final: "+(result1+result2));
 
         }
-    }
+
+        #region prototype of backtrace
+
+        //public static void GenerateAllPasswords(string temp, int pos, int siz, string chars, List<string> result)
+        //{
+        //    if (pos < siz)
+        //    {
+        //        //         cout << "temp= " << temp << " -1\n";     
+        //        for (int i = 0; i < chars.Length; i++)
+        //        {
+        //            GenerateAllPasswords(temp + chars[i], pos + 1, siz, chars, result);
+        //            //         cout << "temp= " << temp << " -3\n";  
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine( "Your password is '" + temp + "'.");
+        //        result.Add(temp);
+        //        //         if (temp==pwd) { return; }
+        //    }
+        //}
+
+        //public static void GenerateAllPasswords(string temp, int pos, int siz, List<string> chars, List<string> result)
+        //{
+        //    if (pos < siz)
+        //    {
+        //        //         cout << "temp= " << temp << " -1\n";     
+        //        for (int i = 0; i < chars.Count; i++)
+        //        {
+        //            GenerateAllPasswords(temp + chars[i]+ "," , pos + 1, siz, chars, result);
+        //            //         cout << "temp= " << temp << " -3\n";  
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Your password is '" + temp + "'.");
+        //        result.Add(temp);
+        //        //         if (temp==pwd) { return; }
+        //    }
+        //}
+        #endregion
+
+
+    };
 }
---
+++ HiddenMarkovModel/backtrack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiddenMarkovModel
+{
+    class Backtrack
+    {
+        private List<string> s;
+        private string[] solution;    // 用來存放一組可能的情形
+        public int T; // n 為現在正在枚舉的維度
+
+
+        public Backtrack(int _T, HashSet<string> _s)
+        {
+            this.T = _T;
+            this.s = _s.ToList<string>();
+            this.solution = new string[T];
+        }
+
+        public List<string> getALLPaths()
+        {
+            List<string> result = new List<string>();
+            string temp = "";
+            GenerateAllPasswords(temp, 0, T, this.s, result);
+            return result;
+        }
+
+
+        public static void GenerateAllPasswords(string temp, int pos, int siz, List<string> chars, List<string> result)
+        {
+            if (pos < siz)
+            {
+                //         cout << "temp= " << temp << " -1\n";     
+                for (int i = 0; i < chars.Count; i++)
+                {
+                    GenerateAllPasswords(temp + chars[i] + ",", pos + 1, siz, chars, result);
+                    //         cout << "temp= " << temp << " -3\n";  
+                }
+            }
+            else
+            {
+                //Console.WriteLine("Your password is '" + temp + "'.");
+                result.Add(temp.Remove(temp.Length-1,1));
+                //         if (temp==pwd) { return; }
+            }
+        }
+
+
+    };
+}
